48c0755064b720f451d9890418574f8cc762cf69
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-04-04 04:43:16 -0300

    Use controlled inputs for login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,34 +1,59 @@
-// The login screen with email and password fields. Trigger login on submit
+// The login screen with email and password fields in a controlled form.
+// Triggers login on submit.
 import React, { Component } from 'react'
 import { View, TextInput } from 'react-native'
 
 export default class LoginScreen extends Component {
+  constructor(params) {
+    super(params)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
   // Focus a field by reference
   focusField = (field) => {
     this.refs[field].focus()
   }
 
+  // Submit current credentials from state
+  submitForm = () => {
+    const { email, password } = this.state
+
+    console.log(`email: ${email}`)
+    console.log(`password: ${password}`)
+  }
+
   render() {
+    const { email, password } = this.state
+
     return (
       <View>
         <TextInput
           ref="email"
+          value={ email }
           placeholder="Enter your email"
           autoFocus
           returnKeyType="next"
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={ false }
+          onChangeText={ (value) => this.setState({ email: value }) }
           onSubmitEditing={ () => this.focusField('password') }
         />
 
         <TextInput
           ref="password"
+          value={ password }
           placeholder="Enter your password"
           secureTextEntry
           returnKeyType="send"
           autoCapitalize="none"
           autoCorrect={ false }
+          onChangeText={ (value) => this.setState({ password: value }) }
+          onSubmitEditing={ () => this.submitForm() }
         />
       </View>
     )
